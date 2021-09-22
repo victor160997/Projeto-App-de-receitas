40389435c7570151bd3d1edd8391c7948d9a5238
Guilherme Viana <guilhermehviana01@gmail.com>
2021-09-22 16:14:49 -0300

    Conclui requisitos footer

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Footer from '../components/Footer';
 import HeaderExplore from '../components/HeaderExplore';
 
 class Perfil extends Component {
@@ -7,6 +8,7 @@ class Perfil extends Component {
       <div>
         <HeaderExplore titlePage="Perfil" />
         Perfil
+        <Footer />
       </div>
     );
   }
