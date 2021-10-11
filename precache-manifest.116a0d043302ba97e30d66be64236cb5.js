2adf0b84899e7539b9e0cd79c41951eda81e1037
Victor Mendonca <victor.men.dev@gmail.com>
2021-10-11 18:52:19 -0300

    Updates

---
+++ precache-manifest.116a0d043302ba97e30d66be64236cb5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "164487b4f8a2d8cecbb9e77c5bf0933d",
+    "url": "/Projeto-App-de-receitas/index.html"
+  },
+  {
+    "revision": "e28fa5cccb6d1bffd496",
+    "url": "/Projeto-App-de-receitas/static/css/main.5aff678c.chunk.css"
+  },
+  {
+    "revision": "442e64d8b06593a62d8a",
+    "url": "/Projeto-App-de-receitas/static/js/2.7e5e763d.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/Projeto-App-de-receitas/static/js/2.7e5e763d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e28fa5cccb6d1bffd496",
+    "url": "/Projeto-App-de-receitas/static/js/main.4dda2550.chunk.js"
+  },
+  {
+    "revision": "39fce3f70b5ed1aa2721",
+    "url": "/Projeto-App-de-receitas/static/js/runtime-main.e2fa595f.js"
+  },
+  {
+    "revision": "50c033bcb4690ee7e3581908e5f80ad4",
+    "url": "/Projeto-App-de-receitas/static/media/back.50c033bc.jpg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/Projeto-App-de-receitas/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/Projeto-App-de-receitas/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/Projeto-App-de-receitas/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/Projeto-App-de-receitas/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/Projeto-App-de-receitas/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/Projeto-App-de-receitas/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/Projeto-App-de-receitas/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/Projeto-App-de-receitas/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
